1919193ee0f0eb3d1975ac3c742fdf205a7ff144
Jack Baker <jackofcablestar@gmail.com>
2019-05-10 21:06:10 +0800

    rename SCounter.java to EnemyCounter.java
    and also add a custom constructor that allows us to specify a number of enemies left

---
+++ EnemyCounter.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * EnemyCounter is a HUD Element for the number of remaining enemies in the world.
+ * 
+ * This actor is notable for being the actor that decides the win condition.
+ * 
+ * @author Kyle, Jack 
+ * @version 0.2.0
+ */
+public class EnemyCounter extends Actor
+{  
+    int EnemiesLeft = 20;
+	
+	public EnemyCounter()
+	{}
+	
+	public EnemyCounter(int el)
+	{
+		//	el is meant to be the number of enemies a world is expected to require the player defeat.
+		this.EnemiesLeft = el;
+	}
+	
+    public void act() 
+    {
+        setImage(new GreenfootImage("" + EnemiesLeft, 25, Color.WHITE, null));
+        
+        if(EnemiesLeft == 0)
+        {
+            Greenfoot.setWorld(new Congratulations());
+        }
+        
+    }    
+    
+    public void removeScore()
+    {
+        this.EnemiesLeft--;
+    }
+}
