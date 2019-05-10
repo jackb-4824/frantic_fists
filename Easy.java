6d06862b4e46d18bd1e353c0b84cc0627648201e
Jack Baker <jackofcablestar@gmail.com>
2019-05-10 23:19:28 +0800

    Refactored worlds for new enemy refactors
    This also fixes Hard's counters not having proper assets and just looking bland

--- Easy.java
+++ Easy.java
@@ -8,46 +8,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Easy extends World
 {
-    //SCORING-ENEMEIS LEFT
-    SCounter sCounter = new SCounter();
-    SCCounter sCCounter = new SCCounter();
-    
-    //HP BARS
-    HPBars hpbars = new HPBars();
+	HPBars hpbars = new HPBars();
+	Player player = new Player(hpbars);
+    EnemyCounter enemyCounter = new EnemyCounter();
+    ScoreCounter scoreCounter = new ScoreCounter();
+	
     /**
      * Constructor for objects of class Easy.
      * 
      */
     public Easy()
     {    
-        
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1200, 400, 1);
         GreenfootImage bg = new GreenfootImage("TestBG.png");
         bg.scale(getWidth(), getHeight());
         setBackground(bg);
-        
-        
+		
         prepare();
     }
-    
-    //ENEMIES LEFT
-    public SCounter getCounter()
-    {
-        return sCounter;
-    }
-    //SCORING
-    public SCCounter getCounter1()
-    {
-        return sCCounter;
-    }
-    
-    public HPBars getHPBars()
-    {
-        return hpbars;
-    }
-    
-    
+	
     public void act()
     {
         spawn();
@@ -57,27 +36,25 @@ public class Easy extends World
     {
         if (Greenfoot.getRandomNumber(1000) < 10) 
         {
-            if (getObjects(Enemy1.class).size() < 10) {
+            if (getObjects(Enemy1.class).size() < 10)
+			{
 				if(Greenfoot.getRandomNumber(1) == 0)
-					addObject(new Enemy1(false, sCounter), 0, 275);
+					addObject(new Enemy1(false, enemyCounter, scoreCounter), 0, 275);
 				else
-					addObject(new Enemy1(true, sCounter), 1199, 275);
+					addObject(new Enemy1(true, enemyCounter, scoreCounter), 1199, 275);
             }
         }
         
         if (Greenfoot.getRandomNumber(1000) < 5) 
         {
-            if (getObjects(Enemy2.class).size() < 10) {  
-                //addObject(new Enemy2(), 0, 275);  
+            if (getObjects(Enemy2.class).size() < 10)
+			{
+				if(Greenfoot.getRandomNumber(1) == 0)
+					addObject(new Enemy2(false, enemyCounter, scoreCounter), 0, 275);
+				else
+					addObject(new Enemy2(true, enemyCounter, scoreCounter), 1199, 275);
             }
         }
-        if (Greenfoot.getRandomNumber(1000) < 3) 
-        {
-            if (getObjects(Enemy3.class).size() < 10) {  
-                //addObject(new Enemy3(), 0, 275);  
-            }
-        }
-        
     }
 
     /**
@@ -88,24 +65,31 @@ public class Easy extends World
     {
         GreenfootImage scoreImage = new GreenfootImage("Score.png");
         GreenfootImage ERImage = new GreenfootImage("EnemiesRemaining.PNG");
-        Player player = new Player(hpbars);
         
         addObject(player,600,275);
         addObject(hpbars, 630, 355);
 
-        //addObject(playerHealth,477,199);
-        //addObject(enemiesLeft,999,54);
-
-        //Enemies Reamining
         getBackground().drawImage(ERImage, 900, 32);
-        addObject(sCounter,1035,71);
+        addObject(enemyCounter,1035,71);
 
-        //Score
         getBackground().drawImage(scoreImage, 700, 32);
-        addObject(sCCounter,768,68);
-        
-
-        
-        
+        addObject(scoreCounter,768,68);   
     }
+	
+//	The following are probably vestigial. (as of 2019-05-10)
+	
+	public EnemyCounter getEC()
+	{
+		return enemyCounter;
+	}
+	
+	public ScoreCounter getSC()
+	{
+		return scoreCounter;
+	}
+	
+	public HPBars getHPBars()
+	{
+		return hpbars;
+	}
 }
--- Medium.java
+++ Medium.java
@@ -8,20 +8,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Medium extends World
 {
-    //SCORING-ENEMEIS LEFT
-    SCounter sCounter = new SCounter();
-    SCCounter sCCounter = new SCCounter();
-    
-    //HP BARS
-    HPBars hpbars = new HPBars();
+	HPBars hpbars = new HPBars();
+	Player player = new Player(hpbars);
+	EnemyCounter enemyCounter = new EnemyCounter();
+	ScoreCounter scoreCounter = new ScoreCounter();
+	
     /**
      * Constructor for objects of class Medium.
      * 
      */
     public Medium()
     {    
-        
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1200, 400, 1);
         GreenfootImage bg = new GreenfootImage("BG2.png");
         bg.scale(getWidth(), getHeight());
@@ -29,24 +26,7 @@ public class Medium extends World
         
         prepare();
     }
-    
-    //ENEMIES LEFT
-    public SCounter getCounter()
-    {
-        return sCounter;
-    }
-    //SCORING
-    public SCCounter getCounter1()
-    {
-        return sCCounter;
-    }
-    
-    public HPBars getHPBars()
-    {
-        return hpbars;
-    }
-    
-    
+	
     public void act()
     {
         spawn();
@@ -56,21 +36,33 @@ public class Medium extends World
     {
         if (Greenfoot.getRandomNumber(1000) < 10) 
         {
-            if (getObjects(Enemy1.class).size() < 10) {  
-                addObject(new Enemy1(), 0, 275);  // TODO: CONSTRUCTORS!
+            if (getObjects(Enemy1.class).size() < 10)
+			{
+				if(Greenfoot.getRandomNumber(1) == 0)
+					addObject(new Enemy1(false, enemyCounter, scoreCounter), 0, 275);
+				else
+					addObject(new Enemy1(true, enemyCounter, scoreCounter), 1199, 275);
             }
         }
         
         if (Greenfoot.getRandomNumber(1000) < 5) 
         {
-            if (getObjects(Enemy2.class).size() < 10) {  
-                //addObject(new Enemy2(), 0, 275);  
+            if (getObjects(Enemy2.class).size() < 10)
+			{
+				if(Greenfoot.getRandomNumber(1) == 0)
+					addObject(new Enemy2(false, enemyCounter, scoreCounter), 0, 275);
+				else
+					addObject(new Enemy2(true, enemyCounter, scoreCounter), 1199, 275);
             }
         }
         if (Greenfoot.getRandomNumber(1000) < 3) 
         {
-            if (getObjects(Enemy3.class).size() < 10) {  
-                //addObject(new Enemy3(), 0, 275);  
+            if (getObjects(Enemy3.class).size() < 10)
+			{
+				if(Greenfoot.getRandomNumber(1) == 0)
+					addObject(new Enemy3(false, enemyCounter, scoreCounter), 0, 275);
+				else
+					addObject(new Enemy3(true, enemyCounter, scoreCounter), 1199, 275);
             }
         }
         
@@ -83,18 +75,32 @@ public class Medium extends World
     private void prepare()
     {
         GreenfootImage scoreImage = new GreenfootImage("Score.png");
-        GreenfootImage ERImage = new GreenfootImage("EnemiesRemaining.PNG");  //TODO: THE FUCKS ALL THIS?
-        Player player = new Player(hpbars);
+        GreenfootImage ERImage = new GreenfootImage("EnemiesRemaining.PNG");
 
         addObject(player,600,275);
-        addObject(hpbars, 200, 40);
+        addObject(hpbars, 630, 355);
 
-        //Enemies Reamining
         getBackground().drawImage(ERImage, 900, 32);
-        addObject(sCounter,1035,71);
+        addObject(enemyCounter,1035,71);
 
-        //Score
         getBackground().drawImage(scoreImage, 700, 32);
-        addObject(sCCounter,768,68);
+        addObject(scoreCounter,768,68);
     }
+	
+//	The following are probably vestigial. (as of 2019-05-10)
+	
+	public EnemyCounter getEC()
+	{
+		return enemyCounter;
+	}
+	
+	public ScoreCounter getSC()
+	{
+		return scoreCounter;
+	}
+	
+	public HPBars getHPBars()
+	{
+		return hpbars;
+	}
 }
